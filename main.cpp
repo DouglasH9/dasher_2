3396fec0133c12bd002e2aba594bf37406d689bb
DouglasH9 <DouglasH9882@gmail.com>
2024-03-27 16:27:46 -0500

    animated all backgrounds

--- main.cpp
+++ main.cpp
@@ -15,7 +15,11 @@ int main() {
     std::vector<Nebula> neb_vec;
 
     Texture2D far_bg = LoadTexture("../textures/far-buildings.png");
+    Texture2D mid_ground = LoadTexture("../textures/back-buildings.png");
+    Texture2D fore_ground = LoadTexture("../textures/foreground.png");
     float bg_x{};
+    float mid_x{};
+    float fore_x{};
 
     for (int i{0}; i < number_of_nebs; ++i) {
         neb_vec.emplace_back(screen_width + (i * 200.0));
@@ -33,11 +37,29 @@ int main() {
         if (bg_x <= -far_bg.width*2.0)
             bg_x = 0.0;
 
+        mid_x -= 60 * dT;
+        if (mid_x <= -mid_ground.width*2.0)
+            mid_x = 0.0;
+
+        fore_x -= 100 * dT;
+        if (fore_x <= -fore_ground.width*2.0)
+            fore_x = 0.0;
+
         //draw background
         Vector2 bg1_pos{bg_x, 0.0};
         DrawTextureEx(far_bg, bg1_pos, 0.0, 4.7, WHITE);
         Vector2 bg2_pos{bg_x + ((float)far_bg.width * 2.0), 0.0};
         DrawTextureEx(far_bg, bg2_pos, 0.0, 4.7, WHITE);
+        //draw mid ground
+        Vector2 mg1_pos{mid_x, 0.0};
+        DrawTextureEx(mid_ground, mg1_pos, 0.0, 4.7, WHITE);
+        Vector2 mg2_pos{mid_x + ((float)mid_ground.width * 2.0), 0.0};
+        DrawTextureEx(mid_ground, mg2_pos, 0.0, 4.7, WHITE);
+        //draw fore ground
+        Vector2 fg1_pos{fore_x, 0.0};
+        DrawTextureEx(fore_ground, fg1_pos, 0.0, 4.7, WHITE);
+        Vector2 fg2_pos{fore_x + ((float)fore_ground.width * 2.0), 0.0};
+        DrawTextureEx(fore_ground, fg2_pos, 0.0, 4.7, WHITE);
 
 
         if (scarfy.get_scarfy_pos_y() >= screen_height - scarfy.get_rec_height()) {
@@ -72,6 +94,8 @@ int main() {
     for (Nebula neb: neb_vec)
         UnloadTexture(neb.get_neb_text());
     UnloadTexture(far_bg);
+    UnloadTexture(mid_ground);
+    UnloadTexture(fore_ground);
     CloseWindow();
 
     return 0;
